9d73ce2bad8eb538d481c760a61cdc66723e02e0
Angela <acsmoot14@gmail.com>
2018-09-20 00:25:53 -0500

    initial pseudocode in games.js and include stylesheets, script declaration

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// * four crystals-Btn displayed as buttons on page.
+
+// * Display a randomNumber at the start of game.
+// * The random number shown at the start of the game should be between 19 - 120.
+    // var randomNumber = Math.floor(Math.random() * 4);
+
+// * When player clicks on a crystals-Btn,  
+//  add a specific amount of points to the player's total score.
+// Each crystal should have a random hidden value between 1 - 12.
+//   * Your game will hide this amount until the player clicks a crystal.
+//   * When click one, update the player's score counter.
+
+// * The player wins if their total score matches the random number from the beginning of the game.
+
+// * The player loses if their score goes above the random number.
+
+// * The game restarts when the player wins or loses.
+
+//  * When the game begins again,
+//  the player should see a new random number.
+//  all the crystals will have four new hidden values.
+//  the user's score (and score counter) will reset to zero.
+
+// * The app should show the number of games the player wins and loses. 
+//do not refresh the page as a means to restart the game.
